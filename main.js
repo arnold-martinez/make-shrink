caed8210fa842dda94b5b7d5b323d97e36ccef3c
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2020-11-10 10:19:33 -0600

    Open window from menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu, globalShortcut } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 // Set env
 process.env.NODE_ENV = 'development'
@@ -7,6 +7,7 @@ const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
 
 let mainWindow
+let aboutWindow
 
 function createMainWindow() {
     mainWindow = new BrowserWindow({
@@ -21,31 +22,51 @@ function createMainWindow() {
     mainWindow.loadFile('./app/index.html')
 }
 
+function createAboutWindow() {
+    aboutWindow = new BrowserWindow({
+        title: 'About ImageShrink',
+        width: 300,
+        height: 300,
+        icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+        resizable: false,
+        backgroundColor: 'white'
+    })
+
+    aboutWindow.loadFile('./app/about.html')
+}
+
 app.on('ready', () => {
     createMainWindow()
 
     const mainMenu = Menu.buildFromTemplate(menu)
     Menu.setApplicationMenu(mainMenu)
-
-    globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload())
-    globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () => mainWindow.toggleDevTools())
-
     mainWindow.on('closed', () => (mainWindow = null))
 })
 
 const menu = [
-    ...(isMac ? [{ role: 'appMenu'}] : []),
-    {
-        role: 'fileMenu'
-        /*label: 'File',
+    ...(isMac ? [{
+        label: app.name,
         submenu: [
             {
-                label: 'Quit',
-                accelerator: 'CmdOrCtrl+W',
-                click: () => app.quit(),
-            },
-        ],*/
+                label: 'About',
+                click: createAboutWindow,
+            }
+        ]
+    }] : []),
+    {
+        role: 'fileMenu'
     },
+    ...(!isMac ? [
+        {
+            label: 'Help',
+            submenu: [
+                {
+                    label: 'About',
+                    click: createAboutWindow,
+                }
+            ]
+        }
+    ]: []),
     ...(isDev
         ? [
             {
